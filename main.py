fa17b716aeae31d5ac4af81732f015de81fcff9e
joe <jooeel777@gmail.com>
2023-09-16 07:10:32 +0530

    Main.py development

--- main.py
+++ main.py
@@ -7,11 +7,39 @@ import sqlite3
 
 flag = True
 
+def databaseConnection(ip,port):
+    db = sqlite3.connect("data.db")
+    cur = db.cursor()
+    global flag
+    if flag:
+        cur.excute("create table log(ip varchar(15), port int(4));")
+        db.commit()
+        flag = False
+    except:
+        flag = False
+    cur.execute(f"insert into log values(\"{ip}\",{port});")
+    db.commit()
+    db.close()
+    print(f"[-] From {ip} to {port}")
+
 class deepPacket:
+
+    def __init__(self):
+        pass
     
-    def filterConnection(packets):
+    def filterConnection(self,packets):
         for packet in packets:
             if packet[2].dport in [21,22,23,80,443,3306]:
                 ip = packet[1].src
                 port = packet[2].dport
                 databaseConnection(ip,port)
+
+    def monitorConnections(self):
+        try:
+            while True:
+                packet = sc.sniff(count = 20, timeout = 5)
+                t = threading.Thread(target = self.filterConnection, args = (packet,) )
+                t.start()
+        except:
+            pass
+
