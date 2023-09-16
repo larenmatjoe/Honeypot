2ef24ba52735761dc551a5213744bde68f87db45
joe <jooeel777@gmail.com>
2023-09-16 08:22:47 +0530

    Testing main.py [updated]

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ def databaseConnection(ip,port):
     db = sqlite3.connect("data.db")
     cur = db.cursor()
     global flag
-    if flag:
-        cur.excute("create table log(ip varchar(15), port int(4));")
-        db.commit()
-        flag = False
+    try:
+        if flag:
+            cur.execute("create table log(ip varchar(15), port int(4));")
+            db.commit()
+            flag = False
     except:
         flag = False
     cur.execute(f"insert into log values(\"{ip}\",{port});")
@@ -23,23 +24,30 @@ def databaseConnection(ip,port):
     print(f"[-] From {ip} to {port}")
 
 class deepPacket:
-
-    def __init__(self):
-        pass
     
-    def filterConnection(self,packets):
+    def filterConnection(packets):
         for packet in packets:
-            if packet[2].dport in [21,22,23,80,443,3306]:
-                ip = packet[1].src
-                port = packet[2].dport
-                databaseConnection(ip,port)
+            try:
+                try:
+                    if packet[2].dport in [21,22,23,80,443,3306]:
+                        ip = packet[1].src
+                        port = packet[2].dport
+                        databaseConnection(ip,port)
+                except AttributeError or ValueError:
+                    pass
+            except IndexError:
+                pass
+            packet = None
 
-    def monitorConnections(self):
+    def monitorConnections():
         try:
-            while True:
-                packet = sc.sniff(count = 20, timeout = 5)
-                t = threading.Thread(target = self.filterConnection, args = (packet,) )
-                t.start()
-        except:
+            try:
+                while True:
+                    packet = sc.sniff(count = 20, timeout = 5)
+                    t = threading.Thread(target = deepPacket.filterConnection, args = (packet,) )
+                    t.start()
+            except AttributeError:
+                pass
+        except ValueError:
             pass
-
+deepPacket.monitorConnections()
