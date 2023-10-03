09b9926a7f98c95ab14318176e5a4901e0108800
joe <jooeel777@gmail.com>
2023-10-03 17:56:35 +0530

    Server Config

--- server.py
+++ server.py
@@ -16,32 +16,34 @@ def authData(ip,port,username,password):
             flag1 = False
     except:
         flag = False
-        cur.execute(f"insert into auth values(\"{ip}\",{port},\"{username}\",\"{password}\");")
-        db.commit()
-        db.close()
+    cur.execute(f"insert into auth values(\"{ip}\",22,\"{username}\",\"{password}\");")
+    db.commit()
+    db.close()
 
 class server:
     def telnet() :
         ip = "127.0.0.1"
         server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        server.bind((ip,23))
-        server.listen(3)
-        connection, address = server.accept()
-        connection.send(b"Welcome to Telnet Server \n")
-        connection.send(b"======================== \n")
-        connection.send(b"Username: ")
-        username = connection.recv(1024)
-        connection.send(b"Password: ")
-        password = connection.recv(2048)
-        time.sleep(2.5)
-        username = username.strip()
-        password = password.strip()
-        username = username.decode()
-        password = password.decode()
-        authData(address[0],address[1],username,password)
-        if username not in ["admin","Admin","root","administrator"]:
-            connection.send(b"Connection revoked: INVALID UESRNAME")
-            connection.close()
-        else:
-            connection.send(b"Connection refused : WRONG PASSWORD")
-            connection.close()
+        server.bind((ip,2323))
+        while True:
+            server.listen(3)
+            connection, address = server.accept()
+            connection.send(b"Welcome to Telnet Server \n")
+            connection.send(b"======================== \n")
+            connection.send(b"Username: ")
+            username = connection.recv(1024)
+            connection.send(b"Password: ")
+            password = connection.recv(2048)
+            time.sleep(2.5)
+            username = username.strip()
+            password = password.strip()
+            username = username.decode()
+            password = password.decode()
+            authData(address[0],address[1],username,password)
+            if username not in ["admin","Admin","root","administrator"]:
+                connection.send(b"Connection revoked: INVALID UESRNAME")
+                connection.close()
+            else:
+                connection.send(b"Connection refused : WRONG PASSWORD")
+                connection.close()
+server.telnet()
